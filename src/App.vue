9c134fd174811d6f2522e90a5ca15d78664e2888
Serdar Ayalp <serdarayalp@googlemail.com>
2021-04-01 13:33:14 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Toast/>
+    <InputText v-model="text"/>
+    <Button label="Senden" icon="pi pi-user" @click="send"></Button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: null
+    }
+  },
+  methods: {
+    send() {
+      this.$toast.add({
+        severity: 'info',
+        summary: 'Hinweis',
+        detail: this.text
+      });
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   //optional for column grouping
+
+import Toast from "primevue/toast";
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+const app = createApp(App)
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
+
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup);
+
+app.mount('#app')
